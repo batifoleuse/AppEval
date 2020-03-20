a8ad49787fbb547dacbccc18adaf49be542195fb
Charles Spielmann <33904991+batifoleuse@users.noreply.github.com>
2020-03-20 09:53:52 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {SectionList, StyleSheet, Text, View} from 'react-native';
+
+import {evalApp} from './app';
+
+export default class AppCall extends Component {
+  state = {
+    listContact: [],
+  };
+  constructor(props) {
+    super(props);
+    this.state.listContact = evalApp;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SectionList
+          sections={[
+            {title: 'D', data: ['Devin', 'Dan', 'Dominic']},
+            {
+              title: 'J',
+              data: ['Jackson', 'John', 'Julie'],
+            },
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => (
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+          )}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
